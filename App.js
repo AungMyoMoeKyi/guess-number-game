7d0ad9d4525e906d6d86a28cac2e5bc53803eaae
AungMyoMoeKyi <aungmyomoekyi@gmail.com>
2021-09-09 00:49:24 +0630

    first upload

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./components/Header.js";
+import StartGameScreen from "./screens/StartGameScreen.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Guess a Number"></Header>
+      <StartGameScreen/>
     </View>
   );
 }
