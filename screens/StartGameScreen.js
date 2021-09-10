39cc7533bf8ad8ce996358f1559ee933b9e23ee4
AungMyoMoeKyi <aungmyomoekyi@gmail.com>
2021-09-10 21:07:14 +0630

    second upload

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,17 +6,68 @@ import {
     Button,
     TouchableWithoutFeedback,
     Keyboard,
+    Alert
 } from "react-native";
 import Card from "../components/Card";
 import Colors from "../constants/colors";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 
 const StartGameScreen = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
 
-    const numberInputHandler = (inputText) => {
-        setEnteredValue(inputText.replace(/[^0-9]g/), "");
+    // const numberInputHandler = (inputText) => {
+    //     setEnteredValue(inputText.replace(/[^0-9]g/), "");
+    // };
+
+    const resetInputHandler = (props) => {
+        setEnteredValue("");
+        setConfirmed(false);
     };
+
+    const confirmedInputHandler = (props) => {
+        const chooseNumber = parseInt(enteredValue);
+        if (chooseNumber === NaN || chooseNumber <= 0 || chooseNumber > 99) {
+            return;
+        }
+
+        setConfirmed(true);
+        setSelectedNumber(chooseNumber);
+        setEnteredValue("");
+    }
+
+    let confirmedOutput;
+    if (confirmed) {
+        confirmedOutput = <Text>Chosen Number : {selectedNumber}</Text>;
+    }
+
+    const confirmInputHandler = () => {
+        const chooseNumber = parseInt(enteredValue);
+        if (isNaN(chooseNumber) || chooseNumber <= 0 || chooseNumber > 99) {
+            Alert.alert(
+                "Invalid number!",
+                "Number has to be a number between 1 and 99. ",
+                [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+            );
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(chooseNumber);
+        setEnteredValue("");
+    };
+
+    // implement at StartGameScreen.js
+    if (confirmed) {
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You Selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)}/>
+            </Card>
+        );
+    }
 
     return (
         <TouchableWithoutFeedback
@@ -36,22 +87,24 @@ const StartGameScreen = (props) => {
                         keyboardType="number-pad"
                         maxLength={2}
                         selectionColor='red'
-                        onChangeText={numberInputHandler}
+                        onChangeText={(inputText) => { setEnteredValue(inputText.replace(/[^0-9]g/), ""); }}
                         value={enteredValue}
                     />
                     <View style={styles.buttonContainer}>
                         <View style={styles.button}>
-                            <Button title="Reset" onPress={() => setEnteredValue("")} color={Colors.accents} />
+                            <Button title="Reset" onPress={resetInputHandler} color={Colors.accents} />
                         </View>
                         <View style={styles.button}>
                             <Button
                                 title="Confirm"
-                                onPress={() => { }}
+                                onPress={confirmInputHandler}
                                 color={Colors.primary}
                             />
                         </View>
                     </View>
                 </Card>
+                {confirmedOutput}
+
             </View>
         </TouchableWithoutFeedback>
     );
@@ -90,6 +143,14 @@ const styles = StyleSheet.create({
         width: 50,
         textAlign: "center",
     },
+
+    // align center
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    // dismiss the keyboard when confirm complete
+
 });
 
 export default StartGameScreen;
